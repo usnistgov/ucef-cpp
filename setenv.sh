6426607ff0e2e5fe65b56cb1889fc796c446a181
Andras Nadas <andrew_n79@yahoo.com>
2016-03-18 14:13:25 -0500

    fixed compilation issues of generated code

--- setenv.sh
+++ setenv.sh
@@ -2,10 +2,10 @@
 
 
 export EXEDIR="$HOME/Projects/C2NG/c2wtng-cpp/examples/HelloWorld/Execution/target/bin-exec"
-export BOOST_LIB_DIR="$HOME/Projects/C2NG/boostlib"
-export RTI_HOME="$HOME/Projects/C2NG/portico-master"
-export RTI_LIB_DIR="$HOME/Projects/C2NG/portico-master/cpp-hla13-macosx"
-export MYSQL_LIB_DIR="$HOME/Projects/C2NG/mysql-connector-c++-1.0.5-osx10.5-x86-64bit"
+export BOOST_LIB_DIR="$HOME/Projects/C2NG/boostlib/lib"
+export BOOST_INC_DIR="$HOME/Projects/C2NG/boostlib/include"
+export RTI_HOME="$HOME/Projects/C2NG/portico-2.0.2"
+export RTI_LIB_DIR="$RTI_HOME/lib/gcc4"
+export MYSQL_LIB_DIR="$HOME/Projects/C2NG/mysql-connector-c++-1.0.5-osx10.5-x86-64bit/lib"
 export DYLD_LIBRARY_PATH=".:$BOOST_LIB_DIR/lib:$RTI_LIB_DIR:$MYSQL_LIB_DIR/lib:$DYLD_LIBRARY_PATH"
 echo "Setting environment variables (BOOST_LIB_DIR, RTI_LIB_DIR, MYSQL_LIB_DIR, etc.)"
-	
