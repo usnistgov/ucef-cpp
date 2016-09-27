fac0b0175e0070ed9370988f3f31d52876284017
Yogesh Barve <ydbarve@isis.vanderbilt.edu>
2016-09-27 18:18:12 +0000

    added build_example script for the Helloworld_simple_cpp

--- examples/HelloWorld_Simple/HelloWorld_Simple_Impl/build_example.sh
+++ examples/HelloWorld_Simple/HelloWorld_Simple_Impl/build_example.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # Author: Yogesh Barve
+
 #export RTI_HOME=/home/vagrant/c2wt-dev/dependency/portico/portico-2.1.0
 export RTI_HOME=/usr/local/portico/portico-2.1.0
 
 mvn_install_deploy() {
 	echo "Maven Compiling...."
-	mvn clean install -U
+	mvn clean install
 	echo "Maven Deploying to the Archiva....."
 	mvn deploy
 	echo "Deployment Completed...."
@@ -19,30 +20,6 @@ traverse_dir(){
 	cd ..
 }
 
-PROJECT_DIR=${PWD}
-
-
-CPP_FOUNDATION_DIR=$PROJECT_DIR/foundation 
-thirdParty=$PROJECT_DIR/3rdparty
-
-echo "Compiling the 3rdParty"
-cd $thirdParty
-traverse_dir "portico-hla"
-traverse_dir "mysql-connector"
-mvn_install_deploy
-cd ..
-
-echo "Entering foundation Directory: " $CPP_FOUNDATION_DIR
-cd $CPP_FOUNDATION_DIR/
-
-
-traverse_dir "core"
-traverse_dir "C2WMySQLLogger"
-traverse_dir "C2WConsoleLogger"
-traverse_dir "rti-base"
-traverse_dir "SynchronizedFederate"
-traverse_dir "OmnetFederate"
-traverse_dir "ExecutionAssembler"
 mvn_install_deploy
 
 echo "=================================================================================="
