95b612977cfcdefac1ffb1253ad58b7ad4ff5b50
himanshu38 <himanshu@isis.vanderbilt.edu>
2016-12-24 15:36:43 -0600

    Updated with latest content from the release repository

--- setenv.sh
+++ setenv.sh
@@ -4,7 +4,7 @@
 export EXEDIR="$HOME/Projects/C2NG/c2wtng-cpp/examples/HelloWorld/Execution/target/bin-exec"
 export BOOST_LIB_DIR="$HOME/Projects/C2NG/boostlib/lib"
 export BOOST_INC_DIR="$HOME/Projects/C2NG/boostlib/include"
-export RTI_HOME="$HOME/Projects/C2NG/portico-2.0.2"
+export RTI_HOME="$HOME/portico-2.1.0"
 export RTI_LIB_DIR="$RTI_HOME/lib/gcc4"
 export MYSQL_LIB_DIR="$HOME/Projects/C2NG/mysql-connector-c++-1.0.5-osx10.5-x86-64bit/lib"
 export DYLD_LIBRARY_PATH=".:$BOOST_LIB_DIR/lib:$RTI_LIB_DIR:$MYSQL_LIB_DIR/lib:$DYLD_LIBRARY_PATH"
