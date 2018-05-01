a33e84b91e9b5ab16930200644afa7b785434eb1
Marty Burns <marty@hypertek.us>
2018-05-01 16:29:32 -0400

    try again

--- setup_foundation.sh
+++ setup_foundation.sh
@@ -1,60 +1,21 @@
 #!/bin/bash
 
-
-mvn_install_deploy() {
-	echo "Maven install..."
-	mvn clean install deploy -U
-}
-
-traverse_dir(){
-	echo ${1}
-	cd ${1} 
-	mvn_install_deploy
-	cd ..
-}
-
 PROJECT_DIR=${PWD}
 CPP_FOUNDATION_DIR=${PROJECT_DIR}/foundation
 THIRD_PARTY=${PROJECT_DIR}/3rdparty
 source $HOME/.bashrc
-
-echo "Compiling 3rd party libraries first"
 echo "Home: $HOME"
-echo env | grep RTI
+env | grep RTI
 
 # 3rdparty
+echo "Compiling 3rd party libraries first"
 cd ${THIRD_PARTY}
-mvn clean install deploy -fae -U
-
-# 3rdparty/portico-hla
-#traverse_dir "portico-hla"
-
-# 3rdparty/mysql-connector
-#traverse_dir "mysql-connector"
-
-# 3rdparty
-#mvn_install_deploy
-
-# .
-cd ${PROJECT_DIR}
+mvn clean install deploy -fae -U -B
 
 # foundation
 echo "Entering foundation Directory: " ${CPP_FOUNDATION_DIR}
 cd ${CPP_FOUNDATION_DIR}
-mvn clean install deploy -fae -U
-
-# foundation/*
-#traverse_dir "core"
-#traverse_dir "C2WMySQLLogger"
-#traverse_dir "C2WConsoleLogger"
-#traverse_dir "config"
-#traverse_dir "rti-base"
-#traverse_dir "SynchronizedFederate"
-#traverse_dir "OmnetFederate"
-#traverse_dir "ExecutionAssembler"
-
-# foundation/
-#mvn_install_deploy
+mvn clean install deploy -fae -U -B
 
 echo "=================================================================================="
 echo "Completed the compilation, installation, deployment of the C2W foundation packages"
