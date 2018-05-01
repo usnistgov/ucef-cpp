d4ffe2b47a235b266c7a2be26e9f8ac64a7b5408
Marty Burns <marty@hypertek.us>
2018-05-01 17:54:09 -0400

    still trying...

--- setup_foundation.sh
+++ setup_foundation.sh
@@ -5,17 +5,17 @@ CPP_FOUNDATION_DIR=${PROJECT_DIR}/foundation
 THIRD_PARTY=${PROJECT_DIR}/3rdparty
 source $HOME/.bashrc
 echo "Home: $HOME"
-env | grep RTI
+echo "RTI_HOME: $RTI_HOME"
 
 # 3rdparty
 echo "Compiling 3rd party libraries first"
 cd ${THIRD_PARTY}
-mvn clean install deploy -fae -U -B
+mvn clean install deploy -fae -U -B -X
 
 # foundation
 echo "Entering foundation Directory: " ${CPP_FOUNDATION_DIR}
 cd ${CPP_FOUNDATION_DIR}
-mvn clean install deploy -fae -U -B
+mvn clean install deploy -fae -U -B -X
 
 echo "=================================================================================="
 echo "Completed the compilation, installation, deployment of the C2W foundation packages"
